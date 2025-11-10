08e39839a10f872a5fbe2d79fab8ed131d471679
mdrmarcela <marcela.rodriguesccomput@gmail.com>
2025-11-09 23:27:48 -0300

    comentando o matrix_generator

--- src/matrix_generator.py
+++ src/matrix_generator.py
@@ -1,17 +1,18 @@
-# src/matrix_generator.py
 import random
 import argparse
 from pathlib import Path
 
+#gera duas matrizes do mesmo tamanho
 def generate(mat_size, out_a, out_b):
     out_a = Path(out_a)
     out_b = Path(out_b)
     out_a.parent.mkdir(parents=True, exist_ok=True)
     out_b.parent.mkdir(parents=True, exist_ok=True)
 
+#Abre os arquivos contendo as matrizes. 
     with open(out_a, "w", encoding="utf8") as fa, open(out_b, "w", encoding="utf8") as fb:
         for i in range(mat_size):
-            row_a = []
+            row_a = [] #Cria duas listas vazias para armazenar as linhas das matrizes A e B.
             row_b = []
             for j in range(mat_size):
                 row_a.append(f"{random.uniform(0.15,1.15):.4f}")
